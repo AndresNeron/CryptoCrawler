3951272d4a5dc8ec6b8d582d6ab9559617327413
NeronNymus <jorge.gonzalez@cimat.mx>
2025-05-31 09:11:58 -0600

    update

---
+++ synchro/synchronize_crypto_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# The intention of this script is to execute all SQL scripts under /tmp/crypto_data
+
+# Function to run a SQL script as the postgres user
+find /tmp/crypto_data -type f | grep -vE 'create.sql|crawled_cryptos_ranked'  | while read script_path;
+do
+	echo "$script_path"
+	#sudo -u postgres psql -f "$script_path"
+done
