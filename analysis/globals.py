f21d6e48455997b7d8f3c6ec1b721238cb842d47
NeronNymus <neron.academico@gmail.com>
2025-06-18 19:39:35 -0600

    update

---
+++ analysis/globals.py
@@ -0,0 +1,11 @@
+import os
+from datetime import datetime
+
+# Global variables
+parent_path = os.getcwd()
+timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+graph_dir = f"{parent_path}/graphs/{timestamp}/backtesting"
+
+# Create needed directories
+os.makedirs(graph_dir, exist_ok=True)
+os.makedirs(f"{globals.parent_path}/graphs/rsi", exist_ok=True)
